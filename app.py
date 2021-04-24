174b722225354331463c85dc3808764214dfa951
keigo0205 <fushio.keigo@gmail.com>
2021-04-25 02:14:10 +0900

    Fix: test

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def callback():
         )
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=event.message.text)
+            TextSendMessage(text="aa")
         )
 
     return 'OK'

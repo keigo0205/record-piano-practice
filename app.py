c03e4303cdc27ab3a4b10056d66ff9c6003aaadd
keigo0205 <fushio.keigo@gmail.com>
2021-04-25 13:43:47 +0900

    test

--- app.py
+++ app.py
@@ -67,12 +67,14 @@ def callback():
             continue
         if not isinstance(event.message, TextMessage):
             continue
-        app.logger.info(str(dir(event.source)))
-        print(dir(event.source))
 
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="11")
+        )
+        line_bot_api.reply_message(
+            event.source.user_id,
+            TextSendMessage(text="22")
         )
 
     return 'OK'
